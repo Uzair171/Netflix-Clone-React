85a4b235c1f02ea50ff0a9727941c2972c688b6a
uzair171 <jabransaleem171@gmail.com>
2025-06-16 18:23:27 +0500

    added the hero banner and started working at movies cards

--- src/components/TitleCards/TitleCard.jsx
+++ src/components/TitleCards/TitleCard.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import './TitleCards.css'
+import cards from '../../assets/cards/Cards_data.js'
+import './TitleCard.css'
+
+  
+ const renderedCards = cards.map((data, index) => (
+  <div className="card" key={index}>
+    <img src={data.image} alt="Movie card" />
+    <p>{data.name}</p>
+  </div>
+));
 
 const TitleCard = () => {
   return (
-    <div className='navbar'>
-      
+    <div className='title-Cards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {renderedCards}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCard
+export default TitleCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar.jsx'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/Play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCard from '../../components/TitleCards/TitleCard.jsx'
 
 const Home=() => {
     return(
         <div className="home">
             <Navbar/>
+            <div className="hero">
+                <img src={hero_banner} alt="hero-banner" className='banner-img' />
+                <div className="hero_caption">
+                    <img src={hero_title} alt="hero-title" className='caption_img'/>
+                    <p>Discovering his ties to a secret ancient order, a young man living
+                        in modren istanbul embarks on a quest to save a city from immortal
+                        enemy 
+                    </p>
+                    <div className="hero-btn">
+                        <button className='btn'><img src={play_icon} alt="Play Icon" />Play</button>
+                        <button className='btn dark-btn'><img src={info_icon} alt="Info Icon" />More Info</button>
+                    </div>
+                </div>
+            </div>
+            <TitleCard/>
         </div>
     )
 }
