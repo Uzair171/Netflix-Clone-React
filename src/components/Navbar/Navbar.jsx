9508e40c182095337f2550e9e77dc2965b81f1c1
uzair171 <jabransaleem171@gmail.com>
2025-06-16 16:41:08 +0500

    first commit netflix navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./Navbar.css"
+import logo from '../../assets/logo.png'
+import search from '../../assets/search_icon.svg'
+import bellIcon from '../../assets/bell_icon.svg'
+import caretIcon from '../../assets/caret_icon.svg'
+import profileImg from '../../assets/profile_img.png'
+ 
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='left-navbar'>
+        <img src={logo} alt="Netflix-Logo" className='main-logo'/>
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse By Language</li>
+        </ul>
+      </div>
+      <div className='right-navbar'>
+        <img src={search} alt="Search Icon" className='search-icon' />
+        <p>Children</p>
+        <img src={bellIcon} alt="Bell Icon" />
+        <div className='navbar-profile'>
+          <img src={profileImg} alt="profile Image" className='profile' />
+          <img src={caretIcon} alt="Caret Image" />
+        </div>
+        <div className='dropdown'>
+          <p>SignOut of Netflix</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/TitleCards/TitleCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCards.css'
+
+const TitleCard = () => {
+  return (
+    <div className='navbar'>
+      
+    </div>
+  )
+}
+
+export default TitleCard
