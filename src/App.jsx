b97891017a5bb9dcd26d55680a5236bf9d3e34b2
uzair171 <jabransaleem171@gmail.com>
2025-06-17 15:49:49 +0500

    player to show trailer or commentry etc about movie according to id

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Home from  "./pages/Home/Home.jsx"
 import { Routes,Route } from "react-router-dom"
 import Login from "./pages/Login/Login.jsx"
+import Player from "./pages/Player/Player.jsx"
 
 export default function App(){
   return(
@@ -9,6 +10,7 @@ export default function App(){
       <Routes>
         <Route path="/" element={ <Home/>}/>
         <Route path="/login" element={ <Login/>}/>
+        <Route path="/player/:id" element={ <Player/>}/>
      
       </Routes>
     </>
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
-import back_arrow_img from '../../assets'
+import back_arrow_img from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOGQ1NWQyNjY1ODQzZTBmYjc3MTdhMWM4MThhOGJiNiIsIm5iZiI6MTc1MDE0NDk4MS42NDYwMDAxLCJzdWIiOiI2ODUxMTdkNTIwNzUwZTI1ZDBiNmRmNDMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.7RD-euAObMG8Rq6aiDYVjMtIDYqDyQlMbCDw7uQyU44'
+  }
+};
+
+
 const Player = () => {
+
+  const {id} = useParams();
+
+  const [apiData, setApiData] = useState({
+    key:'',
+    name:'',
+    published_at:'',
+    type: '',
+  })
+
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results[0]))
+  .catch(err => console.error(err));
+
+  },[])
+
   return (
     <div className='player'>
-      
-
+      <img src={back_arrow_img} alt="Back Arrow Image" />
+      <iframe src={`https://www.youtube.com/embed/${apiData.key}`}
+       frameborder="0" width='90%' height='90%' title='trailor' allowFullScreen ></iframe>
+        <div className='player-info'>
+          <p>{apiData.published_at.slice(0,10)}</p>
+          <p>{apiData.name}</p>
+          <p>{apiData.type}</p>
+        </div>
     </div>
   )
 }
