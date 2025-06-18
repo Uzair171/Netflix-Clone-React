b207b5aff33a9c71e0a97f00f280f21ffdfdd05a
uzair171 <jabransaleem171@gmail.com>
2025-06-18 15:02:15 +0500

    render issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,43 @@
-import  { useEffect, useState} from "react"
-import Home from  "./pages/Home/Home.jsx"
-import { Routes,Route,useNavigate } from "react-router-dom"
-import Login from "./pages/Login/Login.jsx"
-import Player from "./pages/Player/Player.jsx"
-import { onAuthStateChanged } from "firebase/auth"
-import { auth } from "./firebase.js"
-import { ToastContainer } from 'react-toastify';
-import netflix_spinner from './assets/netflix_spinner.gif'
+import { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Player from "./pages/Player/Player.jsx";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase.js";
+import { ToastContainer } from "react-toastify";
+import netflix_spinner from './assets/netflix_spinner.gif';
 
-export default function App(){
-
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(true); 
+export default function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        navigate("/"); // user is logged in
-      } else {
-        navigate("/login"); // user is not logged in
-      }
-      setLoading(false); 
+      setUser(user);
+      setLoading(false);
     });
 
-    return () => unsubscribe(); // clean up
+    return () => unsubscribe();
   }, []);
-  
-  return(
-    loading ? <div className='spinner'>
-      <img src={netflix_spinner} alt="" />
-    </div>:<>
-      <ToastContainer theme="dark"/>
+
+  if (loading) {
+    return (
+      <div className='spinner'>
+        <img src={netflix_spinner} alt="Loading..." />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <ToastContainer theme="dark" />
+
       <Routes>
-        <Route path="/login" element={ <Login/>}/>
-        <Route path="/" element={ <Home/>}/>
-        <Route path="/player/:id" element={ <Player/>}/>
-     
+        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/player/:id" element={user ? <Player /> : <Navigate to="/login" />} />
       </Routes>
     </>
-  )
+  );
 }
