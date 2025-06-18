3c9070c5293fd7c5df48d7a63fb561df29eca2fc
uzair171 <jabransaleem171@gmail.com>
2025-06-18 15:25:16 +0500

    hover issue of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,20 +10,21 @@ import { logOut } from '../../firebase'
 const Navbar = () => {
 
   const darkRef = useRef();
-  useEffect(()=>{
-   window.addEventListener('scroll',()=>{
-    if(window.scrollY >100){
-      darkRef.current.classList.add('nav-dark')
-    }else{
-      darkRef.current.classList.remove('nav-dark')
-    }
-   }) 
-  },[])
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        darkRef.current.classList.add('nav-dark');
+      } else {
+        darkRef.current.classList.remove('nav-dark');
+      }
+    });
+  }, []);
 
   return (
     <div className='navbar' ref={darkRef}>
       <div className='left-navbar'>
-        <img src={logo} alt="Netflix-Logo" className='main-logo'/>
+        <img src={logo} alt="Netflix-Logo" className='main-logo' />
         <ul>
           <li>Home</li>
           <li>TV Shows</li>
@@ -37,16 +38,20 @@ const Navbar = () => {
         <img src={search} alt="Search Icon" className='search-icon' />
         <p>Children</p>
         <img src={bellIcon} alt="Bell Icon" />
-        <div className='navbar-profile'>
-          <img src={profileImg} alt="profile Image" className='profile' />
-          <img src={caretIcon} alt="Caret Image" />
-        </div>
-        <div className='dropdown'>
-          <p onClick={()=> logOut()}>SignOut of Netflix</p>
+
+        {/* Wrapped profile and dropdown */}
+        <div className='profile-wrapper'>
+          <div className='navbar-profile'>
+            <img src={profileImg} alt="Profile" className='profile' />
+            <img src={caretIcon} alt="Caret" />
+          </div>
+          <div className='dropdown'>
+            <p onClick={() => logOut()}>Sign Out of Netflix</p>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
