30ecb767be1f56d6fbcf578c603c0b025d0060d7
uzair171 <jabransaleem171@gmail.com>
2025-06-18 10:15:47 +0500

    firebase package

---
+++ src/firebase.js
@@ -0,0 +1,53 @@
+
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, collection, getFirestore, serverTimestamp, setDoc } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmTksNPCrfhFcBUoG3e-QtvNtWYLjBcwY",
+  authDomain: "netflix-clone-935f1.firebaseapp.com",
+  projectId: "netflix-clone-935f1",
+  storageBucket: "netflix-clone-935f1.firebasestorage.app",
+  messagingSenderId: "790380748773",
+  appId: "1:790380748773:web:74fc8039b0bb341190190c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signUp= async (name,email,password)=>{
+  try {
+    const res = await createUserWithEmailAndPassword(auth,email,password);
+    console.log(res)
+    const user = res.user
+    
+    await addDoc(collection(db,'user'),{
+      uid:user.uid,
+      name,
+      authprovider:'local',
+      email,
+      createdAt: serverTimestamp()
+    })
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+}
+
+const signIn= async (email,password)=>{
+  try {
+    await signInWithEmailAndPassword(auth,email,password);
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+}
+
+const logOut = ()=>{
+  signOut(auth);
+}
+
+export  {auth, db, signIn, signUp, logOut}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,43 @@
 import React, { useState } from 'react'
 import  './Login.css'
 import logo from '../../assets/logo.png'
+import {signIn,signUp} from '../../firebase.js'
 
 const Login = () => {
   const [signState,setSignState] = useState("Sign In")
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  
+  const user_auth = async (e)=>{
+    e.preventDefault();
+    if(signState === "Sign In"){
+      await signIn(email,password)
+    }else{
+      await signUp(name,email,password)
+    }
+  }
+
+  
+
+
+
+
   return (
     <div className='login'>
       <img src={logo} alt="Logo" />
       <div className="login-form">
         <h1>{signState}</h1>
-        <form action="">
-          {signState === "Sign Up" ?<input type="text" placeholder='Enter your Name'/>:<></> }         
-          <input type="email" placeholder='Enter your Email'/>
-          <input type="password" placeholder='Enter your Password'/>
-          <button className='sign-button'>{signState}</button>
+        <form action="{}">
+          {signState === "Sign Up" ?<input  value={name} onChange = {(e)=>setName(e.currentTarget.value)}  type="text" placeholder='Enter your Name'/>:<></> }         
+          <input value={email} onChange = {(e)=>setEmail(e.currentTarget.value)} type="email" placeholder='Enter your Email'/>
+          <input value={password} onChange = {(e)=>setPassword(e.currentTarget.value)} type="password" placeholder='Enter your Password'/>
+          <button className='sign-button' onClick={user_auth}>{signState}</button>
           <div className="form-help">
             <div className="remember">
               
-              <input type="checkbox" /><label>Remeber me</label>
+              {/* <input type="checkbox" /><label>Remeber me</label> */}
             </div>
             <p>Need Help?</p>
           </div>
